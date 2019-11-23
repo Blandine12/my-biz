70df5a5572cfd316dddbc8d992025eafb5e7f49e
silan <siland.dope@yahoo.fr>
2019-11-22 18:49:44 -0800

    new files

--- add-content.js
+++ add-content.js
@@ -1,9 +1,26 @@
-var text;
-var z = confirm("Press this confirm button!");
-if (z == true) {
-    text = "Confirm is pressed!";
-} else {
-    text = "Cancel is pressed!";
+'use strict';
+
+function doStuff() {
+    var travellingTo = prompt('Where are you travelling to?');
+    var howMuch = prompt('How much is the ticket?');
+    var sale;
+    
+    if (howMuch > 1000.00) {
+        sale = '5%!';
+    } else if (howMuch > 800.00) {
+        sale = '2%';
+    } else if (howMuch > 100.00) {
+        sale = '0%';
+    }else{
+        sale = 'nothing'
+    }
+    
+    document.write( '<h3>' + sale + '</h3>');
 }
 
-document.write( '<h3>' + text + '</h3>');
+doStuff();
+
+
+
+
+
